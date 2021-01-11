48176509c67e583a56d0681b3a2ea2d6f3c0b433
dvcoffey <dvcoffey@gmail.com>
2021-01-11 16:12:15 +0000

    init commit

---
+++ scraper.py
@@ -0,0 +1,13 @@
+class JobScrape():
+
+    def __init__():
+        pass
+    
+    def _format_monster():
+        pass
+    
+    def _scrape_site():
+        pass
+
+    def _get_jobs():
+        pass
